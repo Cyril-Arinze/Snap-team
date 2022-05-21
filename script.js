ecdae99fc75b27f193fe4c7f71efadb34ad7b0f7
Cyril Arinze <103698778+Cyril-Arinze@users.noreply.github.com>
2022-05-21 17:30:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 if (window.innerWidth < 480) {
     
-    document.querySelector(".our-img-desktop").classList.add("visually-hidden");
-    document.querySelector(".my-nav2").classList.add("visually-hidden");
-    
+    document.querySelector(".our-img-desktop").classList.add("visually-hidden");    
 }
+
 if (window.innerWidth > 480) {
     
     document.querySelector(".our-img-mobile").classList.add("visually-hidden");
-    document.querySelector(".my-nav1").classList.add("visually-hidden");
 }
 
 
